65dee0475ff53584e57f18c7a1e230def54bb77c
Edgar Alcantara <81432914+EdgarAlcantaraBrend@users.noreply.github.com>
2022-12-14 21:14:17 -0600

    actualizacion9

--- resources/views/layouts/plantilla2.blade.php
+++ resources/views/layouts/plantilla2.blade.php
@@ -8,12 +8,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="{{ asset('img/alcaldia_6.png') }}" type="image/x-icon">
     <!-- Bootstrap CSS -->
-
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap4.min.css">
-
+    
     <link rel="stylesheet" href="librerias/fontawesome/css/all.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -97,17 +96,19 @@
 
                 <div class="col" id="salir">
 
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle-o" aria-hidden="true"></i>
-                      <i class="fa fa-user" aria-hidden="true"></i> {{ Auth::user()->nombre }}
-                  </a>
-
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-
+                  
+                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+                    <i class="fa fa-user" aria-hidden="true"></i> {{ Auth::user()->nombre }}
+                    </a>
+                  
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                       <a class="dropdown-item" style="color: black">Correo: {{ Auth::user()->email}}</a>
 
-                    <a class="dropdown-item" href="{{url('/logout')}}" style="color: black">Cerrar sesión</a>
-                  </div>
-
+                      <a class="dropdown-item" href="{{url('/logout')}}" style="color: black">Cerrar sesión</a>
+                    </div>
+                  
+                  
                 </div>
             </div>
           </div>
@@ -134,7 +135,6 @@
   <!--/.container-->
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://rawcdn.githack.com/bramus/scroll-timeline/b59ed75508d7341f97dc25eccf20ab66231c815a/dist/scroll-timeline.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
@@ -147,12 +147,13 @@
     <script src="{{ asset('js/carrucelAlfabeto.js') }}"></script>
     <script src="{{ asset('js/perfil.js') }}"></script>
     <script src="{{ asset('js/numeracion.js') }}"></script>
-
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="{{ asset('js/alfabeto.js') }}"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-   <script type="text/javascript" src="js/jquery.maphilight.min.js"></script>
-   <script src="{{ asset('js/cuerpo.js') }}"></script>
-   <script src="{{ asset('js/ruleta_js/bootstrap.js') }}"></script>
+    <script type="text/javascript" src="js/jquery.maphilight.min.js"></script>
+    <script src="{{ asset('js/cuerpo.js') }}"></script>
+    <script src="{{ asset('js/ruleta_js/bootstrap.js') }}"></script>
 
 
 
