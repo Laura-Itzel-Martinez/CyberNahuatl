0f40a4ff94f19800ed1bc9f61d6f719c30fa3a1d
Edgar Alcantara <81432914+EdgarAlcantaraBrend@users.noreply.github.com>
2022-12-14 21:41:52 -0600

    actualizacion12

--- resources/views/layouts/plantilla2.blade.php
+++ resources/views/layouts/plantilla2.blade.php
@@ -106,7 +106,7 @@
                       <a class="dropdown-item" style="color: black">Correo: {{ Auth::user()->email}}</a>
 
                       <a class="dropdown-item" href="{{url('/logout')}}" style="color: black">Cerrar sesión</a>
-                    </div>
+                    
                   
                   
                 </div>
@@ -142,16 +142,14 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <script src="{{ asset('js/navbarSistema.js') }}"></script>
     <script src="{{ asset('js/carrucelAlfabeto.js') }}"></script>
     <script src="{{ asset('js/perfil.js') }}"></script>
     <script src="{{ asset('js/numeracion.js') }}"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/alfabeto.js') }}"></script>
-    <script type="text/javascript" src="js/jquery.maphilight.min.js"></script>
+ <script src="{{ asset('js/alfabeto.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+      integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+   <script type="text/javascript" src="js/jquery.maphilight.min.js"></script>
     <script src="{{ asset('js/cuerpo.js') }}"></script>
     <script src="{{ asset('js/ruleta_js/bootstrap.js') }}"></script>
 
